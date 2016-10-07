50e72d39d27f0719d80b8814485fc0a0df43b79e
lelea2 <daomaikhanh1988@yahoo.com>
2016-10-07 12:49:05 -0700

    Update form

--- controllers/polling.js
+++ controllers/polling.js
@@ -1,17 +1,67 @@
 'use strict';
 
 var express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    //Poll Model
+    pollModel = require('../models/polls');
 
 router.route('/')
   .get(function(req, res) {
     //Fetching poll
+    var pollObject = new pollModel();
+    // Calling our model function.
+    pollObject.getAllPolls(function(err, pollResponse) {
+      if (err) {
+        console.log(err);
+        return res.json({
+          'responseCode' : 1,
+          'responseDesc' : pollResponse
+        });
+      }
+      res.json({
+        'responseCode' : 0,
+        'responseDesc' : 'Success',
+        'data' : pollResponse
+      });
+    });
   })
-  .post(function(req, res) {
+  .post(function(req, res) { //Adding new poll question
     //Add new poll
+    var pollObject = new pollModel();
+    // Calling our model function.
+    // We nee to validate our payload here.
+    pollObject.addNewPolls(req.body, function(err, pollResponse) {
+      if(err) {
+        return res.json({
+          'responseCode' : 1,
+          'responseDesc' : pollResponse
+        });
+      }
+      res.json({
+        'responseCode' : 0,
+        'responseDesc' : 'Success',
+        'data' : pollResponse
+      });
+    });
   })
-  .post(function(req, res) {
-    //Update poll
+  .put(function(req, res) {
+    //Update poll answer
+    var pollObject = new pollModel();
+    // Calling our model function.
+    // We need to validate our payload here.
+    pollObject.votePollOption(req.body, function(err, pollResponse) {
+      if(err) {
+        return res.json({
+          'responseCode' : 1,
+          'responseDesc' : pollResponse
+        });
+      }
+      res.json({
+        'responseCode' : 0,
+        'responseDesc' : 'Success',
+        'data' : pollResponse
+      });
+    });
   });
 
 module.exports = router;
