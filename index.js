13c645bff49c01a3ef56b6b5c943b1dc24da1abc
lelea2 <daomaikhanh1988@yahoo.com>
2016-10-07 10:57:54 -0700

    Initial check in

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ var express = require('express'),
     app = express(),
     http = require('http').Server(app),
     router = express.Router(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3001;
+
+// Adding socket.io
+var io = require('socket.io')(http),
+    db = require('./models/db'),
+    feed;
+
+io.on('connection', function(socket) {
+  feed = require('./models/feed')(socket);
+});
+
+var dbModel = new db();
+dbModel.setupDb();
 
 // router.get('/', function(req, res) {
 //   res.send('<h1>HIIII</h1>');
--- view/js/app.js
+++ view/js/app.js
@@ -1,26 +1,30 @@
-var app = angular.module('myApp', ['ngAnimate', 'ngMaterial', 'ngRoute', 'ngMessages']);
+//Privde a default state for an app
+var app = angular.module('myApp', ['ngRoute', 'ngMessages']);
 
 app.factory('socket', function() {
-  var socket = io.connect('http://localhost:3000');
+  var socket = io.connect('http://localhost:3001'); //bind to port that the app is running on
   return socket;
 });
 
-app.config(function($routeProvider, $locationProvider) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  // console.log($routeProvider);
   $routeProvider
-      .when('/', function() {
+      .when('/', {
         templateUrl: 'components/home.html'
       })
-      .when('/create', function() {
+      .when('/create', {
         templateUrl: 'components/create.html'
       })
-      .when('/view', function() {
+      .when('/view', {
         templateUrl: 'components/view.html'
+      })
+      .otherwise({
+        templateUrl: 'components/create.html'
       });
 
   // use the HTML5 History API to remove hash
   $locationProvider.html5Mode(true);
-
-});
+}]);
 
 app.controller('appController', function($scope, $http, socket) {
 
@@ -33,4 +37,15 @@ app.controller('appController', function($scope, $http, socket) {
       $scope.pollData = response.data;
     });
   }
+
+
+  //Polling on websocket
+  socket.on('changeFeed',function(data) {
+    for(var pollCounter = 0 ;pollCounter < $scope.pollData.length; pollCounter++) {
+      if($scope.pollData[pollCounter].id === data.id) {
+        $scope.pollData[pollCounter].polls = data.polls;
+        $scope.$apply();
+      }
+    }
+  });
 });
